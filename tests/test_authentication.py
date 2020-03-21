eaef21849584e0afae0aedac332d3dcac39c02c4
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2020-03-21 13:26:48 +0100

    Add another test to check for authenticated access to endpoint.

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -48,7 +48,29 @@ class TestTokenEndpoint(TestClientRequestsMixin):
         assert_that(access_token).is_not_none()
         assert_that(token_type).is_equal_to('bearer')
 
-    def test_get_user_endpoint_works_only_if_authenticated(self):
+    def test_get_user_endpoint_without_authentication_returns_401(self):
         # Then try to retrieve data without authorization
         response = self.get(url='/users')
         assert_that(response.status_code).is_equal_to(401)
+
+    def test_get_user_endpoint_with_authentication_returns_200(self):
+        username = 'the_username'
+        password = 'the_password'
+        email = 'mail@trvmp.io'
+
+        # Create user first
+        _ = self.create_user(username=username, password=password, email=email)
+
+        # Then try to log in
+        response = self.post(url="/token", data={'username': username, 'password': password})
+        assert_that(response.status_code).is_equal_to(200)
+        access_token = response.json()['access_token']
+
+        # Then try to retrieve data without authorization
+        response = self.get(url='/users', access_token=access_token)
+        assert_that(response.status_code).is_equal_to(200)
+
+        assert_that(response.json()['user_id']).is_not_none()
+        assert_that(response.json()['username']).is_equal_to(username)
+        assert_that(response.json()['email']).is_equal_to(email)
+        assert_that(response.json()['disabled']).is_false()
